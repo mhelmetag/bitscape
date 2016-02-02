2e4ab970792e9829a58c6a510a31660b77cdbd72
Max Helmetag <mhelmetag@gmail.com>
2016-02-02 00:28:59 -0800

    add mirror image generator

--- bitscape/views.py
+++ bitscape/views.py
@@ -14,5 +14,39 @@ def index(request):
     return render(request, 'index.html')
 
 # Generate and return mirror avatar
-def mirror(request, width, height):
-    pass
+
+class ImageForm(forms.Form):
+    """Form to validate requested placeholder image"""
+
+    height = forms.IntegerField(min_value=1, max_value=2000)
+    width = forms.IntegerField(min_value=1, max_value=2000)
+
+    def generate_image(self, image_format='PNG'):
+        """Generate an image of the requested type and return as raw bytes"""
+        height = self.cleaned_data['height']
+        width = self.cleaned_data['width']
+        key = '{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+
+            # def mirror(request, width, height):
+            #     form = ImageForm({'height': height, 'width': width})
+            #     if form.is_valid():
+            #         image = form.generate_image()
+            #         return HttpResponse(image, content_type='image/png')
+            #     else:
+            #         return HttpResponseBadRequest('Invalid Image Request')
+
+            # image = Image.new('RGB', (width, height))
+            # draw = ImageDraw.Draw(image)
+            # text = '{} X {}'.format(width, height)
+            # textwidth, textheight = draw.textsize(text)
+            # if textwidth < width and textheight < height:
+            #     texttop = (height - textheight) // 2
+            #     textleft = (width - textwidth) // 2
+            #     draw.text((textleft, texttop), text, fill=(255, 255, 255))
+            # content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60 * 60)
+        return content
