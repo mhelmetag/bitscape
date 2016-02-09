76c1337eabef3a3fba233ff9dd88b93a30f861a2
Max Helmetag <mhelmetag@gmail.com>
2016-02-09 01:08:39 -0800

    fixed index page

--- bitscape/views.py
+++ bitscape/views.py
@@ -1,7 +1,7 @@
 from io import BytesIO
 import json
 
-from lib.mirror import Mirror
+from bitscape.lib.mirror import Mirror
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
@@ -15,7 +15,7 @@ from django import forms
 #     return hashlib.sha1(content.encode('utf-8')).hexdigest()
 
 # @etag(generate_etag)
-def bitscape(request, image_type, size):
+def bitscape(request, image_type, image_size):
     form = ImageForm({'image_type': image_type, 'image_size': image_size})
     if form.is_valid():
         image = form.serve_image()
@@ -24,19 +24,20 @@ def bitscape(request, image_type, size):
         return HttpResponseBadRequest('Invalid Image Request')
 
 def index(request):
-    # example = reverse('mirror', kwargs={'width': 200, 'height': 200})
-    # context = {
-    #     'example': request.build_absolute_url(example)
-    # }
-    return render(request, 'index.html')
+    example = reverse('bitscape', kwargs={'image_type': 'mirror', 'image_size': 200})
+    context = {
+        'example': request.build_absolute_uri(example)
+    }
+    return render(request, 'index.html', context)
 
 class ImageForm(forms.Form):
     """Form to validate requested placeholder image"""
-    IMAGE_TYPE_CHOICES = (
-        ('mirror', 'mirror')
-    )
+    # TODO: use choise field instead for security
+    # IMAGE_TYPE_CHOICES = (
+    #     ('mirror', 'mirror')
+    # )
 
-    image_type = forms.ChoiceField(IMAGE_TYPE_CHOICES)
+    image_type = forms.CharField(min_length=1, max_length=10)
     image_size = forms.IntegerField(min_value=40, max_value=500)
 
     def serve_image(self):
@@ -45,12 +46,15 @@ class ImageForm(forms.Form):
         image_type = self.cleaned_data['image_type']
         image_size = self.cleaned_data['image_size']
 
+        # TODO: caching
         # key = '{}.{}.{}'.format(width, height, image_format)
         # content = cache.get(key)
         # if content is None:
 
-        if image_type == "mirror":
-            image = Mirror.new(image_size)
+        # TODO: case after choice field
+        # if image_type == "mirror":
+
+        image = Mirror(image_size).generate_image()
 
         content = BytesIO()
         image.save(content, image_format)
--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,6 @@ import bitscape.views
 
 urlpatterns = (
     url(r'^$', bitscape.views.index, name='index'),
-    url(r'^bitscape/(?P<image_type>[a-z]{1,10})x(?P<image_size>[0-9]{2,3})$',
+    url(r'^bitscape/(?P<image_type>[a-z]{1,10})/(?P<image_size>[0-9]{2,3})$',
         bitscape.views.bitscape, name='bitscape')
 )
